49ea2c63d18254e452bea5494329b09d660196b6
Ayush24204 <ayushgupta24204@gmail.com>
2025-03-04 18:30:58 +0530

    Create 02_DataTypes.js

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all the js codes as newer version
+
+// alert(3 + 3) we are using nodes js not brower its runs max with help of html
+// code readability should be maintain
+let name = "Ayush"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof String);
