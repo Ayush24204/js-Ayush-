699efa4b284b6216ab10e4ba0adbbb731b0cabd1
Ayush24204 <ayushgupta24204@gmail.com>
2025-03-04 17:29:42 +0530

    Create 01_variable.js
    about variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=21201
+let accountEmail="Ayush@gmail.com"
+var accountPassword="55555" // maximun time we dont use var because if scope purpose mostly we use let or var
+accountCity="Hyd"
+let accountState 
+// accountId =22222  after const keyword we can't change any inslized value and its not allowed in js
+accountEmail="ayushgupta@123"
+accountPassword="4544654"
+accountCity="Bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
